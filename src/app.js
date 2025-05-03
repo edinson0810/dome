6373ffaa94ad3cbf0732ab6c38edf3662c648cda
edinson0810 <lache0810@hotmail.com>
2025-05-03 16:59:26 -0500

    completado inicio

--- src/app.js
+++ src/app.js
@@ -1,30 +1,26 @@
 
 import './main.css';
-import { router } from './router/router.js';
+import { router } from './router/router';
 const app = document.querySelector("#app");
 const header = document.querySelector("#header");
 const div = document.createElement("div");
-const logo = document.createElement("a");
 const productos = document.createElement("a");
 const categorias = document.createElement("a");
-logo.textContent = "inicio";
+const logo = document.createElement("a");
+logo.textContent = "Inicio";
 productos.textContent = "Productos";
 categorias.textContent = "Categorias";
+logo.setAttribute("href", "#inicio")
 productos.setAttribute("href", '#productos')
 categorias.setAttribute("href", '#categorias')
-logo.setAttribute("href", "#logo")
 header.classList.add('container', 'header')
 div.classList.add('menu')
 logo.classList.add("menu__link")
 productos.classList.add('menu__link')
 categorias.classList.add('menu__link')
 div.append(productos, categorias);
-header.append(logo, div);
+header.append(logo,div);
 
 window.addEventListener('hashchange', () => {
   router(app)
 });
-
-// window.addEventListener('DOMContentLoaded', () => {
-//   router(app)
-// });
--- src/router/router.js
+++ src/router/router.js
@@ -1,20 +1,20 @@
 import { loadView } from "../helpers/loadView";
 import { productoController } from "../views/productos/productoController.js";
-import { categoriaController } from "../views/categorias/categoriaController.js";
-import { inicioController } from "../views/inicio/inicioController.js";
+import { categoriaController } from "../views/categorias/categoriacontroller.js";
+import {inicioController} from "../views/inicio/inicioController.js";
 
 const routes = {
+
   inicio: {
     "template": "inicio/index.html",
     controlador: inicioController
   },
-  
-  productos: {
+   productos: {
     "template": "productos/index.html",
     controlador: productoController
   },
   categorias: {
-    "template": "controladorcategorias/index.html",
+    "template": "categorias/index.html",
     controlador: categoriaController
   }
 };
@@ -31,9 +31,6 @@ controlador()
 }
 
 const matchRoute = (hash) => {
-  // if(hash){
-  //   return routes["inicio"]
-  // }
   for (const route in routes) {
     if (route === hash) {
       return routes[route];
